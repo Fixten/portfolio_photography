b6291dc95ffef514c8ebe551bf318cb4eaa836b5
fixten <fixten@yandex.ru>
2020-12-03 20:23:52 +0000

    base structure

---
+++ src/components/App/index.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="app">
+      <header className="header">Arina Zhenetl</header>
+      <main className="main">
+        <h1>Портфолио</h1>
+        <section>
+          <h2>Галерея</h2>
+        </section>
+      </main>
+      <footer>
+        <h2>Контакты</h2>
+      </footer>
+    </div>
+  );
+}
